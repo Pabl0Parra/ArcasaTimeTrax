f142a7904f5898c4de83529841b1870fdab6d8d6
Pabl0Parra <pabsblue16@gmail.com>
2023-06-07 01:16:35 +0200

    add new stacks

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 import ClockinScreen from "./src/screens/ClockinScreen";
-import Login from "./src/components/Login";
+import BioLogin from "./src/components/BioLogin";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-// other imports...
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="BioLogin" component={BioLogin} />
         <Stack.Screen name="Clockin" component={ClockinScreen} />
       </Stack.Navigator>
     </NavigationContainer>
